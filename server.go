78e8c9f0507910c5bd1f2262e0004c9b0c4c7356
Davi Schmitz <davi.schmitz@hotmail.com>
2024-06-18 19:48:28 -0300

    Update server.go

--- server.go
+++ server.go
@@ -9,6 +9,10 @@ import (
 
 func StartServer() {
 	http.HandleFunc("/produtos", produtosHandler)
+	http.HandleFunc("/carrinhos", createCarrinho).Methods("POST")
+  	http.HandleFunc("/carrinhos/{id}", getCarrinho).Methods("GET")
+  	http.HandleFunc("/carrinhos/{id}", updateCarrinho).Methods("PUT")
+  	http.HandleFunc("/carrinhos/{id}", deleteCarrinho).Methods("DELETE")
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -46,4 +50,69 @@ func getProdutos(w http.ResponseWriter, r *http.Request) {
 		Produtos := produtos
 		json.NewEncoder(w).Encode(Produtos)
 	}
+}
+
+
+func createCarrinho(w http.ResponseWriter, r *http.Request) {
+    var novoCarrinho model.Carrinho
+    _ = json.NewDecoder(r.Body).Decode(&novoCarrinho)
+    novoCarrinho.Id = strconv.Itoa(rand.Intn(1000000)) // Gera um ID aleatório
+
+    carrinhos[novoCarrinho.Id] = novoCarrinho
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(novoCarrinho)
+}
+
+func getCarrinho(w http.ResponseWriter, r *http.Request) {
+    params := mux.Vars(r)
+    id := params["id"]
+
+    carrinho, ok := carrinhos[id]
+
+    if !ok {
+        http.Error(w, "Carrinho não encontrado", http.StatusNotFound)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(carrinho)
+}
+
+func updateCarrinho(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+
+	var carrinhoAtualizado model.Carrinho
+	_ = json.NewDecoder(r.Body).Decode(&carrinhoAtualizado)
+
+	carrinho, ok := carrinhos[id]
+	if !ok {
+			http.Error(w, "Carrinho não encontrado", http.StatusNotFound)
+			return
+	}
+	carrinho.UserId = carrinhoAtualizado.UserId
+	carrinho.ValorTotal = carrinhoAtualizado.ValorTotal
+	carrinho.InfosProduto = carrinhoAtualizado.InfosProduto
+	carrinhos[id] = carrinho
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(carrinho)
+}
+
+func deleteCarrinho(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+
+	_, ok := carrinhos[id]
+	if ok {
+			delete(carrinhos, id)
+	}
+
+	if !ok {
+			http.Error(w, "Carrinho não encontrado", http.StatusNotFound)
+			return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
